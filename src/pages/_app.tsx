4fbeb56a6905d6fc0bfb0a4b67d6029b39a9ae6e
Guilherme Nisiyama de Jesus <guilherme.nisiyama@gmail.com>
2021-02-22 23:16:52 -0300

    Alteração de dependências e inclusão de title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import type { AppProps /* , AppContext */ } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import Head from 'next/head';
 import { store, persistor } from '../store';
-
 import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -11,6 +11,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Component {...pageProps} />
+        <Head>
+          <title>Wiser Educação - Login</title>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta name="theme-color" content="#383E71" />
+        </Head>
         <GlobalStyle />
         <ToastContainer
           position="top-right"
